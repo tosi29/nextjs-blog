42ee6b9b91e06f603335b57111e6c73aadf5a54e
tosi29 <tosi16@gmail.com>
2021-05-04 23:44:45 +0900

    Define and apply Layout component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
+import Layout from '../../components/layout'
 
 const YourComponent = () => (
   <Image
@@ -13,17 +14,17 @@ const YourComponent = () => (
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
       <h1>First Post</h1>
-      <YourComponent />
+    <YourComponent />
       <h2>
         <Link href="/">
-          <a>Back to home</a>
+        <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   )
 }
